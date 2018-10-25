1834632e039b84f9db829876ac2bed928784df1a
Robin Peters <rp@rpeters.dk>
2018-10-25 18:25:18 +0200

    Added minimal logging statements

--- profiler/profile_manager.py
+++ profiler/profile_manager.py
@@ -1,5 +1,6 @@
 import flask
 import datetime
+import logging
 from flask import request
 
 from contextlib import contextmanager
@@ -7,11 +8,14 @@ from contextlib import contextmanager
 
 class ProfileManager():
     def __init__(self, app, mode):
+        self.log = logging.getLogger()
         if mode.lower() == 'debug':
+            self.log.info("Setting up after-request handler to add server timing header")
             app.after_request(ProfileManager._add_header)
 
     def start(self, key):
         if not flask.has_request_context():
+            self.log.debug("No request context available - start timing ignored")
             return
 
         if not hasattr(request, 'context'):
@@ -21,16 +25,20 @@ class ProfileManager():
 
     def stop(self, key):
         if not flask.has_request_context() or not hasattr(request, 'context'):
+            self.log.debug("No request context available - stop timing ignored")
             return
 
-        key = key.replace(' ', '-')
-        if not key in request.context:
+        _key = key.replace(' ', '-')
+        if not _key in request.context:
+            self.log.warn("Key '{}' not found in request context".format(key))
             return
 
         stop_time = datetime.datetime.now()
-        start_time = request.context.get(key, {}).get('start')
+        start_time = request.context.get(_key, {}).get('start')
         if start_time:
-            request.context[key] = (stop_time - start_time).total_seconds() * 1000
+            request.context[_key] = (stop_time - start_time).total_seconds() * 1000
+        else:
+            self.log.warn("No start time found for key '{}'".format(key))
 
     @contextmanager
     def time(self, key):
